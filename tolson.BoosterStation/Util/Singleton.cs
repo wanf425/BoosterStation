2fbf7e7a169dbe691e056d3e8497edde690f5b04
TOLSONWANG <fss437000@163.com>
2024-07-16 20:27:58 +0800

    添加类注释

--- tolson.BoosterStation/Util/Singleton.cs
+++ tolson.BoosterStation/Util/Singleton.cs
@@ -6,6 +6,18 @@ using System.Threading.Tasks;
 
 namespace tolson.BoosterStation.Util
 {
+    /// <summary>
+    /// 单例类，所有继承此类的子类，通过subClass.Instance获取单例对象。
+    /// 建议子类将构造函数定义为private。
+    /// 已考虑多线程并发场景。
+    /// 1）通过Lazy<T>类型保证线程安全。
+    ///    Lazy<T> 类型的默认构造函数使用 LazyThreadSafetyMode.ExecutionAndPublication 模式，
+    ///    这意味着 Lazy<T> 实例会确保在多线程环境中 Value 属性的初始化过程是安全的，
+    ///    并且初始化的值会被所有线程看到。
+    /// 2）通过(T)Activator.CreateInstance(typeof(T), true);创建实现类,
+    ///    这样Singleton的子类可以将构造函数定义为private,
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public abstract class Singleton<T> where T : class
     {
         private static readonly Lazy<T> instance = new Lazy<T>(CreateInstanceOfT);
