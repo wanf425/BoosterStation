e6f7283b49713fa94d4c4dcd6b8c18453cdde2d8
TOLSONWANG <fss437000@163.com>
2024-07-18 19:27:00 +0800

    添加UI工具类 UISwitch暂时用不上

---
+++ tolson.BoosterStation/Util/UIUtil.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tolson.BoosterStation.Util
+{
+    public class UIUtil
+    {
+        /// <summary>
+        /// 在parentControl中切换childControl
+        /// </summary>
+        /// <param name="parentControl"></param>
+        /// <param name="childControl"></param>
+        public static void UISwitch(Control parentControl, Control childControl)
+        {
+            // 填充整个父控件，Size不匹配时，会自动缩放
+            childControl.Dock = DockStyle.Fill;
+
+            // 保证线程安全
+            if(parentControl.InvokeRequired)
+            {
+                parentControl.Invoke(new Action(() => AddChildControl(parentControl, childControl)));
+            } else
+            {
+                AddChildControl(parentControl, childControl);
+            }
+        }
+
+        private static void AddChildControl(Control parentControl, Control childControl)
+        {
+            parentControl.Controls.Clear();
+            parentControl.Controls.Add(childControl);
+        }
+    }
+}
